4a9b124c4a4a44ba771fd5914ec40d8c12bc5d93
Alunos <sabinomariaeduarda123@gmail.com>
2022-05-03 08:46:02 -0300

    aula web2

--- controller/Form.php
+++ controller/Form.php
@@ -2,6 +2,7 @@
 class Form
 {
   private $message = "";
+  private $error = " ";
   public function __construct()
   {
     Transaction::open();
@@ -9,6 +10,10 @@ class Form
   public function controller()
   {
     $form = new Template("view/form.html");
+    $form->set("id", "");
+    $form->set("estilo","");
+    $form->set("cor", "");
+    $form->set("tamanho", "");
     $this->message = $form->saida();
   }
   public function salvar()
@@ -20,18 +25,71 @@ class Form
         $estilo = $conexao->quote($_POST["estilo"]);
         $cor = $conexao->quote($_POST["cor"]);
         $tamanho = $conexao->quote($_POST["tamanho"]);
-        $resultado = $sapato->insert("estilo, cor, tamanho", "$estilo, $cor, $tamanho");
+        if (empty($_POST["id"])) {
+          $sapato->insert(
+            "estilo, cor, tamanho",
+            "$estilo, $cor, $tamanho"
+          );
+        } else {
+          $id = $conexao->quote($_POST["id"]);
+          $sapato->update(
+            "estilo = $estilo, cor = $cor, tamanho = $tamanho",
+            "id = $id"
+          );
+        }
+        $this->message = $sapato->getMessage();
+        $this->error = $sapato->getError();
       } catch (Exception $e) {
-        echo $e->getMessage();
+        $this->message = $e->getMessage();
+        $this->error = true;
       }
+    }else{
+      $this->message = "Campos não informados!";
+      $this->error = true;
     }
   }
-  public function getMessage()
+  public function editar()
   {
+    if (isset($_GET["id"])) {
+      try {
+        $conexao = Transaction::get();
+        $id = $conexao->quote($_GET["id"]);
+        $sapato = new Crud("sapato");
+        $resultado = $sapato->select("*", "id = $id");
+        if(!$sapato->getError()){
+         $form = new Template("view/form.html");
+         foreach ($resultado[0] as $cod => $valor) {
+           $form->set($cod, $valor);
+        }
+        $this->message = $form->saida();
+      } else {
+        $this->message = $sapato->getMessage();
+        $this->error = true;
+      }
+    } catch (Exception $e) {
+      $this->message = $e->getMessage();
+      $this->error = true;
+    }
+  }
+}
+public function getMessage()
+{
+  if (is_string($this->error)) {
     return $this->message;
+  } else {
+    $msg = new Template("view/msg.html");
+    if ($this->error) {
+      $msg->set("cor", "danger");
+    } else {
+      $msg->set("cor", "success");
+    }
+    $msg->set("msg", $this->message);
+    $msg->set("uri", "?class=Tabela");
+    return $msg->saida();
   }
-  public function __destruct()
-  {
-    Transaction::close();
-  }
+}
+public function __destruct()
+{
+  Transaction::close();
+}
 }
--- controller/Tabela.php
+++ controller/Tabela.php
@@ -2,35 +2,64 @@
 class Tabela
 {
   private $message = "";
+  private $error = "";
   public function __construct()
   {
     Transaction::open();
   }
   public function controller()
   {
-    Transaction::get();
-    $sapato = new Crud("sapato");
-    $resultado = $sapato->select();
-    $tabela = new Template("view/tabela.html");
-    if (is_array($resultado)) {
-      $tabela->set("linha", $resultado);
-      $this->message = $tabela->saida();
+    try {
+      Transaction::get();
+      $sapato = new Crud("sapato");
+      $resultado = $sapato->select();
+      $tabela = new Template("view/tabela.html");
+      if (is_array($resultado)) {
+        $tabela->set("linha", $resultado);
+        $this->message = $tabela->saida();
+      } else {
+        $this->message = $sapato->getMessage();
+        $this->error = $sapato->getError();
+      }
+    } catch (Exception $e) {
+      $this->message = $e->getMessage();
+      $this->error = true;
     }
   }
   public function remover()
   {
-    try {
-      $conexao = Transaction::get();
-      $id = $conexao->quote($_GET["id"]);
-      $sapato = new Crud("sapato");
-      $resultado = $sapato->delete("id = $id");
-    } catch (Exception $e) {
-      echo $e->getMessage();
+    if (isset($_GET["id"])) {
+      try {
+        $conexao = Transaction::get();
+        $id = $conexao->quote($_GET["id"]);
+        $sapato = new Crud("sapato");
+        $sapato->delete("id = $id");
+        $this->message = $sapato->getMessage();
+        $this->error = $sapato->getError();
+      } catch (Exception $e) {
+        $this->message = $e->getMessage();
+        $this->error = true;
+      }
+    } else {
+      $this->message = "Faltando parâmetro!";
+      $this->error = true;
     }
   }
   public function getMessage()
   {
-    return $this->message;
+    if (is_string($this->error)) {
+      return $this->message;
+    } else {
+      $msg = new Template("view/msg.html");
+      if ($this->error) {
+        $msg->set("cor", "danger");
+      } else {
+        $msg->set("cor", "success");
+      }
+      $msg->set("msg", $this->message);
+      $msg->set("uri", "?class=Tabela");
+      return $msg->saida();
+    }
   }
   public function __destruct()
   {
